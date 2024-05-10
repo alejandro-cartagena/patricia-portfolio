412865d145fd521eabe608c7f89619dcdf0fcea3
Ale <alejandrocartagena68@gmail.com>
2024-05-09 21:59:43 -0400

    Made the burger menu work

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 const burgerMenu = document.querySelector(".fa-bars")
+const closeMenu = document.querySelector(".fa-x")
+const offScreenMenu = document.querySelector(".off-screen-menu")
+const heroImage = document.querySelector(".hero__image")
 
+// Function to handle toggling of closeMenu icon visibility
+function toggleCloseMenuVisibility() {
+    if (window.innerWidth > 750) {
+        closeMenu.classList.add("hidden");
+        heroImage.classList.remove("hidden")
+    }
+    else if (window.innerWidth < 750 && offScreenMenu.classList.contains("active")) {
+        closeMenu.classList.remove("hidden");
+        heroImage.classList.add("hidden")
+    }
+}
 
 burgerMenu.addEventListener("click", () => {
-    console.log("Burger menu clicked!")
+    offScreenMenu.classList.add("active")
+    closeMenu.classList.remove("hidden")
+    heroImage.classList.add("hidden")
 })
+
+closeMenu.addEventListener("click", () => {
+    offScreenMenu.classList.remove("active")
+    closeMenu.classList.add("hidden")
+    heroImage.classList.remove("hidden")
+})
+
+window.addEventListener("resize", toggleCloseMenuVisibility);
